0dc3ec3d7ae4305559245bc7bf246d1ab5b5cf36
Mattanapol Konguthaikul <mattanapol.k@draycir.com>
2018-02-11 23:41:14 +0700

    Update material model

--- Estimation.Domain/Models/Material.cs
+++ Estimation.Domain/Models/Material.cs
@@ -6,5 +6,22 @@ namespace Estimation.Domain.Models
 {
     public class Material
     {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public decimal ListPrice { get; set; }
+
+        public decimal NetPrice { get; set; }
+
+        public decimal OfferPrice { get; set; }
+
+        public decimal Manpower { get; set; }
+
+        public decimal Fittings { get; set; }
+
+        public decimal Supporting { get; set; }
+
+        public decimal Painting { get; set; }
     }
 }
