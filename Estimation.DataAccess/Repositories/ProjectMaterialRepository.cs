0e9400a20ad8abc17a7afc3022401f5d4f7b2283
Mattanapol Konguthaikul <mattanapol.k@draycir.com>
2018-03-09 00:46:15 +0700

    Add project material repository

---
+++ Estimation.DataAccess/Repositories/ProjectMaterialRepository.cs
@@ -0,0 +1,47 @@
+﻿using Estimation.DataAccess.Models;
+using Estimation.Domain.Models;
+using Estimation.Services.Interfaces;
+using Estimation.Services.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estimation.DataAccess.Repositories
+{
+    /// <summary>
+    /// Project material repository
+    /// </summary>
+    public class ProjectMaterialRepository : BaseProjectRepository, IProjectMaterialRepository
+    {
+        IProjectMaterialGroupRepository _projectMaterialGroupRepository;
+
+        public ProjectMaterialRepository(ProjectDbContext projectDbContext,
+                                    ITypeMappingService typeMappingService,
+                                    IProjectMaterialGroupRepository projectMaterialGroupRepository): base(projectDbContext, typeMappingService)
+        {
+            _projectMaterialGroupRepository = projectMaterialGroupRepository ?? throw new ArgumentNullException(nameof(projectMaterialGroupRepository));
+        }
+
+        /// <summary>
+        /// Create material and add it to project accordingly
+        /// </summary>
+        /// <param name="materialGroupId"></param>
+        /// <param name="material"></param>
+        /// <returns></returns>
+        public async Task<Material> CreateMaterial(int materialGroupId, Material material)
+        {
+            var projectMaterialGroup = await _projectMaterialGroupRepository.GetProjectMaterialGroup(materialGroupId);
+            if (projectMaterialGroup == null)
+                throw new KeyNotFoundException($"Project material group id = {projectMaterialGroup} is not exist.");
+            
+            var projectMaterialDb = TypeMappingService.Map<Material, ProjectMaterialDb>(material);
+            projectMaterialDb.MaterialType = projectMaterialGroup.MaterialType;
+            DbContext.Material.Add(projectMaterialDb);
+
+            await DbContext.SaveChangesAsync();
+
+            return TypeMappingService.Map<ProjectMaterialDb, Material>(projectMaterialDb);
+        }
+    }
+}
--- Estimation.Services/Interfaces/Repositories/IProjectMaterialRepository.cs
+++ Estimation.Services/Interfaces/Repositories/IProjectMaterialRepository.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Estimation.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Estimation.Services.Interfaces.Repositories
 {
+    /// <summary>
+    /// Interface of Project material repository
+    /// </summary>
     public interface IProjectMaterialRepository
     {
+        /// <summary>
+        /// Create material and add to material group id accordingly
+        /// </summary>
+        /// <param name="materialGroupId"></param>
+        /// <param name="material"></param>
+        /// <returns></returns>
+        Task<Material> CreateMaterial(int materialGroupId, Material material);
     }
 }
