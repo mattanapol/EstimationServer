cbe19edaab5661af9f19f375b706d9b94a3a93d3
Mattanapol Konguthaikul <mattanapol.k@draycir.com>
2018-06-15 23:42:38 +0700

    Fix update material group.

--- Estimation.Domain/Dtos/ProjectMaterialGroupUpdateIncomingDto.cs
+++ Estimation.Domain/Dtos/ProjectMaterialGroupUpdateIncomingDto.cs
@@ -8,6 +8,14 @@
         public string GroupCode { get; set; }
 
         /// <summary>
+        /// Gets or sets the order.
+        /// </summary>
+        /// <value>
+        /// The order.
+        /// </value>
+        public int Order { get; set; }
+
+        /// <summary>
         /// Group name
         /// </summary>
         public string GroupName { get; set; }
